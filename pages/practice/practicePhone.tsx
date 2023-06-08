7095661780a0c289ff60be4867349c4cb749562b
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-06-08 22:15:57 +0900

    slide practice

---
+++ pages/practice/practicePhone.tsx
@@ -0,0 +1,100 @@
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+function Card() {
+  return (
+    <>
+      <div className="card bg-white w-[200px] h-[350px] m-2 rounded-lg shadow-lg">
+        <div className="top">
+          <img
+            className="w-[200px] h-[200px] object-cover  p-2"
+            src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204"
+            alt="img"
+          />
+        </div>
+        <div className="bottom flex flex-col justify-center items-start p-3 bg-">
+          <div className="title font-semibold text-xs my-1">
+            Apple iPhone 13 (128GB) - Midnight
+          </div>
+          <div className="category text-xs font-light my-1">
+            5.4 cm (6.1-inch) display1
+          </div>
+
+          <div className="pricing flex items-center">
+            {" "}
+            <div className="price ">$1000</div>
+            <div className="ml-2 text-xs ">
+              $<del>1320</del>
+            </div>
+          </div>
+          <div className="flex items-center my-2">
+            <button className="border px-3 py-1 text-xs rounded-lg mr-1 ">
+              Buy Now
+            </button>
+            <button className="border px-3 py-1 text-xs rounded-lg ">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Carousel() {
+  const scrollLeft = () => {
+    document.getElementById("content").scrollLeft -= 400;
+  };
+  const scrollRight = () => {
+    document.getElementById("content").scrollLeft += 400;
+  };
+
+  return (
+    <div className="relative">
+      <div className="text-center py-4  text-xl font-bold">Carousel</div>
+      <div className="absolute right-0 top-5 ">
+        <button onClick={scrollLeft} className="p-2 m-2 rounded-full bg-white">
+          <FiChevronLeft />
+        </button>
+        <button onClick={scrollRight} className="p-2 m-2 rounded-full bg-white">
+          <FiChevronRight />
+        </button>
+      </div>
+      <div
+        id="content"
+        className=" carausel p-4 flex items-center justify-start overflow-x-auto scroll-smooth scrollbar-hide"
+      >
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
     // pages폴더/모든다이렉토리/모든파일/확장자
 
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
   ],
+
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+
+  plugins: [require("@tailwindcss/forms"), require("autoprefixer")],
 };
 // 어느 컴포넌트(혹은 page)에 tailwind를 사용할 것인지 설정
